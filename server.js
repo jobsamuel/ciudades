87d6cde3cc53810887b45831adbd7dc5c6d6ea0d
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2015-02-12 13:03:42 -0430

    Nuevo set de datos

--- server.js
+++ server.js
@@ -10,24 +10,13 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/comments.json', function(req, res) {
-  fs.readFile('comments.json', function(err, data) {
+app.get('/venezuela.json', function(req, res) {
+  fs.readFile('venezuela.json', function(err, data) {
     res.setHeader('Content-Type', 'application/json');
     res.send(data);
   });
 });
 
-app.post('/comments.json', function(req, res) {
-  fs.readFile('comments.json', function(err, data) {
-    var comments = JSON.parse(data);
-    comments.push(req.body);
-    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(comments));
-    });
-  });
-});
-
 app.listen(app.set('port'), function() {
-	console.log("Server listening on port: " + app.set('port') )
+	console.log("Server running on http://localhost:" + app.set('port') );
 });
